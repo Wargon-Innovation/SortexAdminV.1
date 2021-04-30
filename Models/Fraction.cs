979ded0c05a64f44c41dea19c675332ef64994f6
Edwin Andersson Hallsten <edan0003@student.hv.se>
2021-04-30 10:37:24 +0200

    FractionNumber

--- Models/Fraction.cs
+++ Models/Fraction.cs
@@ -8,6 +8,7 @@ namespace SortexAdminV._1.Models
     public class Fraction
     {
         public int Id { get; set; }
+        public int Number { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
 
